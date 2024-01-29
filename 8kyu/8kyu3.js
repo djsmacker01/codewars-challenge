38e8b07dec2ef460c8d7b03250e769b86f1ee270
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-01-29 21:16:09 +0000

    codewars challenge 3

---
+++ 8kyu/8kyu3.js
@@ -0,0 +1,11 @@
+// We need a function that can transform a number (integer) into a string.
+
+// What ways of achieving this do you know?
+
+// Examples (input --> output):
+
+function numberToString(num) {
+  // Return a string of the number here!
+  
+  return String(num)
+}
