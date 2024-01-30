44a905c2053f66747898d9e212e1f24f0629adfd
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-01-30 21:22:44 +0000

    modified

--- 8kyu/8kyu6.js
+++ 8kyu/8kyu6.js
@@ -1,9 +1,9 @@
-Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+// Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
 
-Examples:
-1: -1
-14: -14
--34: 34
+// Examples:
+// 1: -1
+// 14: -14
+// -34: 34
 
 function opposite(number) {
   return(-number);
