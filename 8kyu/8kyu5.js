e44639772b7f6837729ffdcd96f5339479fae76c
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-01-30 20:47:56 +0000

    modified

--- 8kyu/8kyu5.js
+++ 8kyu/8kyu5.js
@@ -1,4 +1,13 @@
-const rps = (p1, p2)=> {
+// Rock Paper Scissors
+// Let's play! You have to return which player won! In case of a draw return Draw!.
+
+// Examples(Input1, Input2 --> Output):
+
+// "scissors", "paper" --> "Player 1 won!"
+// "scissors", "rock" --> "Player 2 won!"
+// "paper", "paper" --> "Draw!"
+
+const rps = (p1, p2) => {
   if (p1 === p2) {
     return "Draw!";
   }
