02b10e5a6e9ce9943efd3de1aa15b80783240080
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-01-30 20:45:05 +0000

    New file

---
+++ 8kyu/8kyu5.js
@@ -0,0 +1,18 @@
+const rps = (p1, p2)=> {
+  if (p1 === p2) {
+    return "Draw!";
+  }
+  else if (
+    (p1 === "scissors" && p2 === "paper") ||
+    (p1 === "paper" && p2 === "rock") ||
+    (p1 === "rock" && p2 === "scissors")
+  ) {
+    return "Player 1 won!";
+  } else {
+    return "Player 2 won!";
+  }
+}
+
+// console.log(rps("scissors", "paper")); // Output: "Player 1 won!"
+// console.log(rps("scissors", "rock"));  // Output: "Player 2 won!"
+// console.log(rps("paper", "paper"));    // Output: "Draw!"
