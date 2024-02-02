e9628e8928725d57450935d13c9580332461f4fa
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-02-02 23:53:40 +0000

    new commit

--- 8kyu/8kyu7.js
+++ 8kyu/8kyu7.js
@@ -6,5 +6,6 @@
 // "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
 function covertToArray(value) {
     let covertString = " I love Liverpool Football Club"
-    
- }
+    let res = covertString.split(",")
+}
+ 
