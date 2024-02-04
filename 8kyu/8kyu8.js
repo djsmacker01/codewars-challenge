4d3af39f0dc1d3c80770b9734196de1c93bf69fe
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-02-04 20:49:23 +0000

    new commit

--- 8kyu/8kyu8.js
+++ 8kyu/8kyu8.js
@@ -1,4 +1,10 @@
 // It's pretty straightforward.
-// Your goal is to create a function that removes the first and last characters of a string. 
-// You're given one parameter, the original string. 
+// Your goal is to create a function that removes the first and last characters of a string.
+// You're given one parameter, the original string.
 // You don't have to worry about strings with less than two characters.
+//UsinG the PREP Approach
+// Input are single string with atleast two characters
+// the same string return without the first and last characters
+function removeChar(str) {
+return str.slice()
+}
